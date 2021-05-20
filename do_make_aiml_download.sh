a98f472d187e2bda501b5b777b4f5baa9d647794
David Liebman <radiodee1@gmail.com>
2021-05-20 13:32:02 -0400

    add aiml_download.sh

---
+++ do_make_aiml_download.sh
@@ -0,0 +1,7 @@
+mkdir -p data
+cd data
+
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/aiml-en-us-foundation-alice/aiml-en-us-foundation-alice.v1-9.zip
+
+unzip aiml-en-us-foundation-alice.v1-9.zip
+
