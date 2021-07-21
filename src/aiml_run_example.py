a5022802e25cb59e04a6447933386d7b9f3c1e7c
David Liebman <radiodee1@gmail.com>
2021-07-21 09:36:16 -0400

    add some flags to aiml_run_example.py

--- src/aiml_run_example.py
+++ src/aiml_run_example.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import aiml
+import argparse
 
+parser = argparse.ArgumentParser(description="Bert Aiml", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--generated', action='store_true', help='use generated aiml.')
+parser.add_argument('--startup', action='store_true', help='use startup xml.')
 
+args = parser.parse_args()
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 kernel.verbose(True)
-#kernel.learn("../maze/generated.aiml")
-kernel.learn("../aiml/startup.xml")
+if args.generated:
+    kernel.learn("../maze/generated.aiml")
+if args.startup:
+    kernel.learn("../aiml/startup.xml")
 #kernel.respond("load aiml b")
 
 # Press CTRL-C to break this loop
